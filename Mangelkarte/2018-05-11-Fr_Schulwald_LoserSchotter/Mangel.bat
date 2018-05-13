51c02ea7177488561e8db317b1d699fc881fea16
Jens Spille <jens.spille@web.de>
2018-05-13 21:44:20 +0200

    create html

--- Mangelkarte/2018-05-11-Fr_Schulwald_LoserSchotter/Mangel.bat
+++ Mangelkarte/2018-05-11-Fr_Schulwald_LoserSchotter/Mangel.bat
@@ -23,16 +23,16 @@ sed s/tschuess/%lda%-%lti%/g %tempfname%.md >%tempfname%.tmp2
 rem replace mermaid mit PanDoc Mermais
 rem ==================================
 rem Soll: ```{.mermaid format=svg  theme=forest caption="ADFC Hemmingen/Pattensen"}
-sed "s/mermaid/\{.mermaid format=svg\}/" %tempfname%.tmp2 >%tempfname%.tmp
+rem sed "s/mermaid/\{.mermaid format=svg\}/" %tempfname%.tmp2 >%tempfname%.tmp
 
 rem set pandoc options
 rem ===================
 set panoptions=%panoptions% -t html5
 set panoptions=%panoptions% -s
 rem set panoptions=%panoptions% -S
-set panoptions=%panoptions% -c github-pandoc.css
+set panoptions=%panoptions% -c ..\..\github-pandoc.css
 set panoptions=%panoptions% -M date="%lda% %lti%"
-set panoptions=%panoptions% -F mermaid-filter.cmd
+rem set panoptions=%panoptions% -F mermaid-filter.cmd
 rem set panoptions=%panoptions% --toc
 rem set panoptions=%panoptions% --number-sections
 
