bd5eabf55029aecc567fd707e335751751c880f0
jspille <jens.spille@web.de>
2020-09-23 21:07:58 +0200

    change gitCMDs

--- _gitCMDs.bat
+++ _gitCMDs.bat
@@ -8,31 +8,28 @@
 :: utf-8
 chcp 65001
 
+:: UNIX variables like LC_ALL in order to display Polish (or other UTF-8 characters) correctly.
 set LC_ALL=C.UTF-8
 
 cd .
 set PROMPT=$G
 SET PATH=%PATH%;c:\Program Files\Git
 
-@echo on
-rem Status
-doskey gs=git status
+rem Status 
+doskey gs==echo %CD% $t git status
 doskey gl=git log --graph --pretty=format:"%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr) %%C(bold blue)<%%an>%%Creset" --abbrev-commit -n 16
 
-rem add & commit use <gc with comment>
 doskey ga=git add -A
-doskey gc=git commit -am "$*"
+doskey gc=git commit -am "$*" $t:: use gc with comment
 
-rem push
 doskey gp=git push
 
-rem Git repository browser
-doskey gk=gitk
+doskey gk=gitk $t:: Git repository browser
 
-doskey k=doskey /macros
+doskey k=doskey /macros $B sort
 doskey x=exit
 
+@echo on
+doskey /macros | sort
+
 @cmd.exe /K
-
-
-
