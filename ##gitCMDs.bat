3d8d866f60f0ee806c820d95f25a7b756ccb8210
jspille <jens.spille@web.de>
2020-10-05 19:46:24 +0200

    zoom auf Pattensen u. Hemmingen

--- ##gitCMDs.bat
+++ ##gitCMDs.bat
@@ -10,7 +10,9 @@ chcp 65001
 
 :: UNIX variables like LC_ALL in order to display Polish (or other UTF-8 characters) correctly.
 set LC_ALL=C.UTF-8
-
+title gitCMDs
+color 0b &:: 0Black 1Blue 2Green 3Aqua 4Red 5Purple 6Yellow 7White 8Gray 9Light blue a:Light green b:Light aqua c:Light red d:Light purple e:Light yellow f:Bright white
+mode con cols=140 lines=50
 cd .
 set PROMPT=$G
 SET PATH=%PATH%;c:\Program Files\Git
@@ -28,6 +30,8 @@ doskey gk=gitk $t:: Git repository browser
 
 doskey k=doskey /macros $B sort
 doskey x=exit
+
+doskey f=C:\DataProg\PortProg\FirefoxPortable\FirefoxPortable.exe https://adfc-hemmingen-pattensen.github.io/
 
 @echo on
 doskey /macros | sort &:: Sortierte Ausgabe
